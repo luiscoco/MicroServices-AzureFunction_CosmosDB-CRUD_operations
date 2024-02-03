bf80aaa789cec447df6091c04928edbf0bb88ab1
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2024-02-03 16:23:22 +0100

    adding create item function

---
+++ CreateFunction.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using System;
+
+namespace CosmosDbCrudFunctions
+{
+    public static class CreateFunction
+    {
+        private static readonly string EndpointUri = Environment.GetEnvironmentVariable("CosmosDbEndpointUri");
+        private static readonly string PrimaryKey = Environment.GetEnvironmentVariable("CosmosDbPrimaryKey");
+        private static readonly string DatabaseName = "ToDoList";
+        private static readonly string ContainerName = "Items";
+        private static CosmosClient cosmosClient = new CosmosClient(EndpointUri, PrimaryKey);
+
+        [Function("CreateItem")]
+        public static async Task<HttpResponseData> CreateItem(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            var logger = executionContext.GetLogger("CreateItem");
+            logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+            Container container = cosmosClient.GetContainer(DatabaseName, ContainerName);
+            await container.CreateItemAsync(data, new PartitionKey(data.id.ToString()));
+
+            var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+            await response.WriteStringAsync("Item created successfully");
+
+            return response;
+        }
+    }
+}
